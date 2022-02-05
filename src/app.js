89985a00d5d61395a0e43cc71ec36a8a5ecee781
Учень12 <artems555666777@gmail.com>
2022-02-05 18:56:14 +0200

    finnaly3

--- src/app.js
+++ src/app.js
@@ -27,18 +27,18 @@ class App {
     // this.camera.position.set(0, 0, 4)
     this.camera = new THREE.PerspectiveCamera(50,
         window.innerWidth / window.innerHeight, 0.1, 100)
-    this.camera.position.set( 0, 1.6, 3 )
+    this.camera.position.set(0, 1.6, 3)
 
     this.scene = new THREE.Scene()
     this.scene.background = new THREE.Color(0x505050)
 
     // const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3)
-    const ambient = new THREE.HemisphereLight( 0x606060, 0x404040, 1)
+    const ambient = new THREE.HemisphereLight(0x606060, 0x404040, 1)
     this.scene.add(ambient)
 
     const light = new THREE.DirectionalLight(0xfffff)
     // light.position.set(0.2, 1, 1)
-    light.position.set( 1, 1, 1 ).normalize()
+    light.position.set(1, 1, 1).normalize()
     this.scene.add(light)
 
     this.renderer = new THREE.WebGLRenderer({antialias: true})
@@ -63,14 +63,14 @@ class App {
     this.forDebugOnly()
     this.loadGltf()
     this.setupVR()
-  this.clock = new THREE.Clock()
+    this.clock = new THREE.Clock()
     this.renderer.setAnimationLoop(this.render.bind(this))
 
     window.addEventListener('resize', this.resize.bind(this))
   }
 
-  random( min, max ){
-    return Math.random() * (max-min) + min;
+  random(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
   initSceneCube() {
@@ -83,40 +83,40 @@ class App {
 
     this.scene.add(this.mesh)
 
-    const geometrySphere = new THREE.SphereGeometry( .7, 32, 16 )
-    const materialSphere = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
-    const sphere = new THREE.Mesh( geometrySphere, materialSphere )
-    this.scene.add( sphere )
+    const geometrySphere = new THREE.SphereGeometry(.7, 32, 16)
+    const materialSphere = new THREE.MeshBasicMaterial({color: 0xffff00})
+    const sphere = new THREE.Mesh(geometrySphere, materialSphere)
+    this.scene.add(sphere)
 
     // sphere.position.set(1.5, 0, 0)
   }
 
   forDebugOnly() {
-    const geometrySphere = new THREE.SphereGeometry( .5, 32, 16 )
-    const materialSphere = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
-    const sphere = new THREE.Mesh( geometrySphere, materialSphere )
+    const geometrySphere = new THREE.SphereGeometry(.5, 32, 16)
+    const materialSphere = new THREE.MeshBasicMaterial({color: 0xffff00})
+    const sphere = new THREE.Mesh(geometrySphere, materialSphere)
     sphere.position.set(0, 1.5, -2)
-    this.scene.add( sphere )
+    this.scene.add(sphere)
   }
 
   initScene() {
     this.radius = 0.08
 
     this.movableObjects = new THREE.Group();
-    this.scene.add( this.movableObjects);
+    this.scene.add(this.movableObjects);
 
     this.room = new THREE.LineSegments(
         new BoxLineGeometry(6, 6, 6, 10, 10, 10),
         new THREE.LineBasicMaterial({color: 0x808080})
     )
     this.room.geometry.translate(0, 3, 0)
-   // this.scene.add(this.room)
+    // this.scene.add(this.room)
 
     const geometry = new THREE.IcosahedronBufferGeometry(this.radius, 2)
 
     for (let i = 0; i < 0; i++) {
 
-      const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff } ))
+      const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff}))
 
       object.position.x = this.random(-2, 2)
       object.position.y = this.random(0, 2)
@@ -129,7 +129,8 @@ class App {
 
     }
     this.highlight = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-      color: 0xFFFFF, side: THREE.BackSide}))
+      color: 0xFFFFF, side: THREE.BackSide
+    }))
     this.highlight.scale.set(1.2, 1.2, 1.2)
     this.scene.add(this.highlight)
 
@@ -143,7 +144,7 @@ class App {
         officeChairGlb,
         (gltf) => {
           self.chair = gltf.scene
-          self.chair.scale.set(.4,.4,.4)
+          self.chair.scale.set(.4, .4, .4)
           // self.chair.scale.set(1,1,1)
           // self.chair.scale = new THREE.Vector3(.2,.2,.2)
           self.scene.add(gltf.scene)
@@ -166,12 +167,12 @@ class App {
 
   setupVR() {
     this.renderer.xr.enabled = true
-    document.body.appendChild( VRButton.createButton( this.renderer ) )
+    document.body.appendChild(VRButton.createButton(this.renderer))
 
     let i = 0
-        //this.AxeController(i++)
+    //this.AxeController(i++)
     // this.flashLightController(i++)
-     this.buildStandardController(i++)
+    this.buildStandardController(i++)
     // this.buildStandardController(i++)
 
     //his.buildDragController(i++)
@@ -180,38 +181,52 @@ class App {
     //this.controllers[i] = new FlashLightController(this.renderer, i++, this.scene, this.movableObjects, this.highlight)
     // this.buildStandardController(i++)
 
-  }
-
-  createUI(){
-    const config = {
-      panelSize: { height: 0.8 },
-      height: 500,
-      body: { type: "text" }
+    if (this.controllers.length > 1) {
+      this.leftUi = this.createUI()
+      this.leftUi.mesh.position.set(-.6, 1.5, -1)
+      this.rightUi = this.createUI()
+      this.rightUi.mesh.position.set(.6, 1.5, -1)
+    } else {
+      this.leftUi = this.createUI()
     }
-    const ui = new CanvasUI( { body: "" }, config );
+  }
+  createUI() {
+    const config = {
+      panelSize: {height: 0.8},
+      height: 500,
+      body: {type: "text"}
+    }
+    const ui = new CanvasUI({body: ""}, config);
     ui.mesh.position.set(0, 1.5, -1);
-    this.scene.add( ui.mesh );
+    this.scene.add(ui.mesh);
     return ui;
   }
 
-  updateUI(){
-    if (!this.buttonStates) {
+  updateUI(ui, buttonStates) {
+    if (!buttonStates) {
       return
     }
 
-    const str = JSON.stringify( this.buttonStates, null, 2);
-    if (this.strStates === undefined || ( str != this.strStates )){
-      this.ui.updateElement( 'body', str );
-      this.ui.update();
-      this.strStates = str;
+    const str = JSON.stringify(buttonStates, null, 2);
+    if (!ui.userData || ui.userData.strStates === undefined
+        || (str != ui.userData.strStates)) {
+      ui.updateElement('body', str);
+      ui.update();
+      if (!ui.userData) {
+        ui.userData = {}
+      }
+      ui.userData.strStates = str;
     }
   }
+
+
+
   createButtonStates(components) {
     const buttonStates = {}
     this.gamepadIndices = components
     Object.keys(components).forEach(key => {
       if (key.includes('touchpad') || key.includes('thumbstick')) {
-        buttonStates[key] = { button: 0, xAxis: 0, yAxis: 0 }
+        buttonStates[key] = {button: 0, xAxis: 0, yAxis: 0}
       } else {
         buttonStates[key] = 0
       }
@@ -243,48 +258,45 @@ class App {
     }
   }
 
-  onConnectedRight( event, self){
+  onConnect(event, self) {
     // const self = this
     const info = {};
 
 
-    fetchProfile( event.data, DEFAULT_PROFILES_PATH, DEFAULT_PROFILE ).then( ( { profile, assetPath } ) => {
-      console.log( JSON.stringify(profile));
+    fetchProfile(event.data, DEFAULT_PROFILES_PATH, DEFAULT_PROFILE)
+        .then(({profile, assetPath}) => {
+        //console.log(JSON.stringify(profile));
 
-      info.name = profile.profileId;
-      info.targetRayMode = event.data.targetRayMode;
+        info.name = profile.profileId;
+        info.targetRayMode = event.data.targetRayMode;
 
-      Object.entries( profile.layouts ).forEach( ( [key, layout] ) => {
-        const components = {};
-        Object.values( layout.components ).forEach( ( component ) => {
-          components[component.rootNodeName] = component.gamepadIndices;
+        Object.entries(profile.layouts).forEach(([key, layout]) => {
+          const components = {};
+          Object.values(layout.components).forEach((component) => {
+            components[component.rootNodeName] = component.gamepadIndices;
+          });
+          info[key] = components;
         });
-        info[key] = components;
+
+        if (event.data.handedness === 'left') {
+          self.createButtonStates(info.left);
+        } else {
+          self.createButtonStates(info.right);
+        }
       });
+    }
 
-      self.createButtonStates( info.right );
-
-      console.log( JSON.stringify(info) );
-
-    } );
-  }
-
-  showDebugText() {
-    const dt = this.clock.getDelta()
-
+  showDebugText(dt) {
     if (this.renderer.xr.isPresenting) {
-      const self = this
-      if (this.controllers) {
-        this.controllers.forEach(controller => controller.handle())
-      }
-      if (this.elapsedTime == undefined) {
-        this.elapsedTime = 0
-      }
       this.elapsedTime += dt
       if (this.elapsedTime > 0.3) {
-        this.updateGamepadState()
         this.elapsedTime = 0
-        this.updateUI()
+        if (this.controllers.length > 0) {
+          this.updateUI(this.leftUi, this.controllers[0].buttonStates)
+        }
+        if (this.controllers.length > 1) {
+          this.updateUI(this.rightUi, this.controllers[1].buttonStates)
+        }
       }
     } else {
       // this.stats.update()
@@ -492,9 +504,9 @@ class App {
 
 
     // controller.addEventListener( 'connected', function (event) {
-    //   self.onConnectedRight.call(self, event.data, this)
+    //   self.onConnect.call(self, event.data, this)
     // })
-    controller.addEventListener( 'connected', event => this.onConnectedRight(event, self))
+    controller.addEventListener( 'connected', event => this.onConnect(event, self))
 
     controller.handle = () => this.handleController(controller)
 
